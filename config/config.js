99b9380872acba54c3d48a0a23a6197b78b9ae6e
René Hernández <rhernandez@sistemasesco.com>
2024-11-13 03:34:13 -0300

    fix: update config with correct team and org names

--- config/config.js
+++ config/config.js
@@ -1,7 +1,7 @@
 module.exports = {
     GITHUB_TOKEN: 'YOUR-TOKEN',
     ENTERPRISE: 'ENTERPRISE',
-    TEAM_SLUG: 'TEAM_SLUG',
-    ORG: 'ORG',
+    TEAM_SLUG: 'primary-idtech',
+    ORG: 'primary-idtech',
     API_VERSION: '2022-11-28'
 };
